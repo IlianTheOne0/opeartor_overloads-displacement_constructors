c204dadb7a66113a2b0f2341e76ce426c356dcfc
Ilian Shchepinskyi <iliantheone@gmail.com>
2025-02-06 00:04:15 +0200

    Interim result

---
+++ Domain/Entities/House/interface.h
@@ -0,0 +1,16 @@
+#ifndef HOUSE_INTERFACE_H
+#define HOUSE_INTERFACE_H
+
+#include "../Flat/flat.h"
+
+template<typename TReturn>
+class IHouse
+{
+	virtual size_t getCounter() const = 0;
+	virtual Flat* getObject(const size_t& id) = 0;
+
+	virtual bool addFlat(AFlat& flat) = 0;
+	virtual bool deleteFlet(const size_t& id) const = 0;
+};
+
+#endif
